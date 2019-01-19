ea9809ce9cd9b1fd1474a22604bcc677080148c7
Samuel Berthe <dev@samuel-berthe.fr>
2019-01-19 20:00:07 +0100

    Improves docker logs

--- scripts/init-git-config.sh
+++ scripts/init-git-config.sh
@@ -3,6 +3,8 @@
 git config --global user.email "dev@samuel-berthe.fr"
 git config --global user.name "Samuel Berthe"
 
+git reset --hard
+
 # use credentials from env
 git remote set-url origin https://${GITHUB_USER}:${GITHUB_OAUTH_TOKEN}@github.com/${GITHUB_USER}/powEUr.git
 
