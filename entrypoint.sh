1cd81a0e8ea60d8798b2910d7623242ff3487348
Samuel Berthe <dev@samuel-berthe.fr>
2019-01-19 21:30:14 +0100

    Improved security

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,7 @@ env > /etc/environment
 # https://stackoverflow.com/questions/43802109/output-of-tail-f-at-the-end-of-a-docker-cmd-is-not-showing/43807880#43807880
 echo > /var/log/cron.log
 
+# for python server
+mkdir /tmp/foobar
+
 exec "$@"
