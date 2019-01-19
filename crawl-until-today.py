c108ec843d960ccf2d8cfcaa1ea1a0284a1f51be
Samuel Berthe <dev@samuel-berthe.fr>
2019-01-19 21:35:34 +0100

    Oops

--- crawl-until-today.py
+++ crawl-until-today.py
@@ -12,6 +12,6 @@ now = datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0)
 
 while date < now:
     for country_code in DOMAIN_MAPPINGS.keys():
-        print("Getting generation data: {} / {}".format("FR", date))
-        handle_errors(lambda: generation(date, "FR"))
+        print("Getting generation data: {} / {}".format(country_code, date))
+        handle_errors(lambda: generation(date, country_code))
     date += timedelta(days=1)
